74c453fc6470364926a48392dcb2a5f251e27af5
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-11-13 18:30:46 +0100

    feat(ui): install and configure PrimeNG; update gallery styles with Tailwind utilities

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,6 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GalleryComponent } from './components/gallery/gallery';
 
-import { ApplicationConfig } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
-
 @Component({
   selector: 'app-root',
   imports: [GalleryComponent],
@@ -16,18 +11,3 @@ import Aura from '@primeuix/themes/aura';
 export class App {
   protected readonly title = signal('image-gallery');
 }
-export const appConfig: ApplicationConfig = {
-    providers: [
-        provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
-    ]
-};
-
-// deberia poner el providers este dentro del decorador?
-// antes he hecho un button y no me ha funcionado, todo instalado correctamente y nada
-  // npm list primeng
-  // npm list tailwindcss
