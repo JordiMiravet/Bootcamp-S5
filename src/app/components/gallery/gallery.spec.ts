ddab2b883de69145d44bb9749ca9ad329542deaf
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-11-15 20:13:57 +0100

    test(components/gallery): handleDelete removes the image when confirmation dialog returns true

--- src/app/components/gallery/gallery.spec.ts
+++ src/app/components/gallery/gallery.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GalleryComponent } from './gallery';
+import { ImageModel } from '../../models/imageModel';
 
 describe('Gallery', () => {
   let component: GalleryComponent;
@@ -46,5 +46,34 @@ describe('Gallery', () => {
 
     expect(emptyImage).toBeTruthy();
     expect(emptyImage.textContent).toContain("Imagen no disponible");
+  });
+  
+  it("should delete an image when confirmed", () => {
+
+    const imagePath : string = "https://picsum.photos/";
+    const imageWidth : string = "400/600";
+    const imageType : string = "?grayscale&seed=";
+
+    const mockImages: ImageModel[] = [
+      { id: 1, url: `${imagePath}${imageWidth}${imageType}1`, description: 'Imagen 1', featured: true },
+      { id: 2, url: `${imagePath}${imageWidth}${imageType}2`, description: 'Imagen 2' }
+    ];
+
+    component.images.set(mockImages);
+    fixture.detectChanges();
+
+    const deleteButton = fixture.nativeElement.querySelector('button');
+    expect(deleteButton).toBeTruthy();
+    
+    const confirmSpy = spyOn(window, 'confirm').and.returnValue(true); 
+
+    component.handleDelete(mockImages[1]);
+    fixture.detectChanges();
+
+    expect(confirmSpy).toHaveBeenCalledWith('¿Estás seguro de que deseas eliminar esta imagen?');
+
+    let imagesAfterDelete = component.images(); 
+    expect(imagesAfterDelete.length).toBe(1); 
+    expect(imagesAfterDelete[0].id).toBe(1);
   });
 });
